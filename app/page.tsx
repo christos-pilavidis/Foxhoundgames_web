c216b4c2c79f038b1334868e22447caa2c4d7b06
christos-pilavidis <chris.pilavidis@hotmail.com>
2024-05-13 15:21:14 +0300

    app

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+import { serviceSectionData } from '@/data/service-section/v1/home-page';
+import { Footer } from '@/src/layout/footer/v1';
+import { MainHeader } from '@/src/layout/header';
+import { CtaSection } from '@/src/sections/cta/v1';
+import { Hero } from '@/src/sections/hero/v1';
+import { ServiceSection } from '@/src/sections/service/v1';
+import { Metadata } from 'next';
+import CookieConsent from '@/src/components/cookies/cookiesconsent';
+
+export const metadata: Metadata = {
+  title: 'FoxHound',
+  description: 'Foxhound Games',
+};
+
+export default function Page() {
+  return (
+    <>
+      <MainHeader version="1" />
+      <Hero mediaSrc="/assets/images/hero/Main3.mp4" />
+      <ServiceSection className="!pb-0" {...serviceSectionData} />
+      <CtaSection />
+      <Footer />
+      <CookieConsent />
+    </>
+  );
+}
