be1aa6ac6b0e9cb14c14a06366a7e14b1a0d5f13
christos-pilavidis <chris.pilavidis@hotmail.com>
2024-05-14 14:19:24 +0300

    data

---
+++ data/layout/header/v1/index.ts
@@ -0,0 +1,10 @@
+import { HeaderProps } from '@/src/layout/header/desktop/v1';
+import { menuItemsProps } from '../menu';
+
+export const headerData: HeaderProps = {
+  menuItems: menuItemsProps,
+  contactInfo: {
+    title: 'Need help?',
+    email: 'info@foxhoundgames.com', // Replace with your actual email
+  },
+};
